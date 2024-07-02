776ec42d6baff6802f2f0bfcfb2e0aed87c9c374
TaeSan Choi <xotks7524@gmail.com>
2024-07-02 20:58:12 +0900

    Refactor: 사용자 인증 함수 변경 및 종속성 업데이트
    - 사용자 인증 함수 변경
      - get_current_user에서 get_current_user_id로 변경하여 사용자 ID 가져오는 방식 통일
    - FastAPI 종속성 업데이트
      - add_nutrient 및 get_nutrient 엔드포인트에서 종속성으로 get_current_user_id 사

--- back/routers/nutrient_router.py
+++ back/routers/nutrient_router.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException
 from models.nutrient_data import Nutrient
 from controllers.nutrient_controller import add_nutrient_to_db, get_nutrients_from_db
-from routers.user import get_current_user
+from controllers.auth_controller import get_current_user_id
 
 router = APIRouter()
 
 @router.post("/add_nutrient/")
-def add_nutrient(nutrient: Nutrient, user_id: str = Depends(get_current_user)):
+def add_nutrient(nutrient: Nutrient, user_id: str = Depends(get_current_user_id)):
     return add_nutrient_to_db(nutrient)
 
 @router.get("/get_nutrient/")
-def get_nutrient(user_id: str = Depends(get_current_user)):
+def get_nutrient(user_id: str = Depends(get_current_user_id)):
     return get_nutrients_from_db(user_id)

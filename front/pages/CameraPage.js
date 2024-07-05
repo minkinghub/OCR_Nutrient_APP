ac06f3ca708f84030a65262c4e6ba675ff48738d
minkinghub <kp141@naver.com>
2024-07-05 11:55:39 +0900

    camera

--- front/pages/CameraPage.js
+++ front/pages/CameraPage.js
@@ -1,48 +1,136 @@
-import React, { useState } from 'react';
-import { Button, Image, View } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import axios from 'axios';
+// import React, { useState } from 'react';
+// import { Button, Image, View } from 'react-native';
+// import * as ImagePicker from 'expo-image-picker';
+// import axios from 'axios';
+
+// const CameraPage = () => {
+//   const [selectedImage, setSelectedImage] = useState(null);
+//   const [base64Image, setBase64Image] = useState(null);
+
+//   const pickImage = async () => {
+//     let result = await ImagePicker.launchImageLibraryAsync({
+//       mediaTypes: ImagePicker.MediaTypeOptions.Images,
+//       allowsEditing: true,
+//       aspect: [4, 3],
+//       quality: 1,
+//       base64: true, // base64 인코딩
+//     });
+
+//     if (!result.canceled) {
+//       setSelectedImage(result.assets[0].uri);
+//       setBase64Image(result.assets[0].base64); // base64 데이터 설정.
+//     }
+//   };
+
+//   const uploadImage = async () => {
+//     if (!base64Image) return;
+
+//     const user_id = localStorage.getItem('user_id'); // 로컬 스토리지에서 user_id 가져오기
+
+//     try {
+//       const response = await axios.post('http://127.0.0.1:8000/upload', { base64: base64Image, user_id: user_id });
+//       console.log(response.data);
+//     } catch (error) {
+//       console.error(error.response ? error.response.data : error.message);
+//     }
+//   };
+
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Button title="Pick an image from camera roll" onPress={pickImage} />
+//       {selectedImage && <Image source={{ uri: selectedImage }} style={{ width: 200, height: 200 }} />}
+//       <Button title="Upload Image" onPress={uploadImage} />
+//     </View>
+//   );
+// }
+
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import { useState, useRef } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 
 const CameraPage = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [base64Image, setBase64Image] = useState(null);
+  const [facing, setFacing] = useState('back');
+  const [permission, requestPermission] = useCameraPermissions();
+  const cameraRef = useRef(null);
+  const [photo, setPhoto] = useState(null);
 
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-      base64: true, // base64 인코딩
-    });
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
 
-    if (!result.canceled) {
-      setSelectedImage(result.assets[0].uri);
-      setBase64Image(result.assets[0].base64); // base64 데이터 설정.
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
+  async function takePicture() {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      setPhoto(photo);
     }
-  };
-
-  const uploadImage = async () => {
-    if (!base64Image) return;
-
-    const user_id = localStorage.getItem('user_id'); // 로컬 스토리지에서 user_id 가져오기
-
-    try {
-      const response = await axios.post('http://127.0.0.1:8000/upload', { base64: base64Image, user_id: user_id });
-      console.log(response.data);
-    } catch (error) {
-      console.error(error.response ? error.response.data : error.message);
-    }
-  };
+  }
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {selectedImage && <Image source={{ uri: selectedImage }} style={{ width: 200, height: 200 }} />}
-      <Button title="Upload Image" onPress={uploadImage} />
+    <View style={styles.container}>
+      <CameraView style={styles.cameraSmall} facing={facing} ref={cameraRef}>
+      </CameraView>
+      {photo && <Image source={{ uri: photo.uri }} style={styles.preview} />}
+      <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={takePicture}>
+            <Text style={styles.text}>Take Picture</Text>
+          </TouchableOpacity>
+        </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cameraSmall: {
+    width: 200, // 너비를 조절하여 크기를 줄입니다.
+    height: 200, // 높이를 조절하여 크기를 줄입니다.
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: "green"
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  preview: {
+    width: 200,
+    height: 200,
+    alignSelf: 'center',
+    marginTop: 20,
+  },
+});
 
 module.exports = {
     CameraPage
--- front/pages/GraphPage.js
+++ front/pages/GraphPage.js
@@ -9,205 +9,313 @@ const userId = "66861ec2d90427eb49eda019";
 const labelSize = screenWidth * 0.9 * 0.095;
 
 const GraphPage = () => {
-    const [nutrient, setNutrient] = useState({});  // 서버에서 받아온 영양소 정보
-    const [tdee, setTdee] = useState(1);  // 서버에서 받아온 tdee
-    const [isLoading, setIsLoading] = useState(true);
+    return <></>
+}
+//     const [nutrient, setNutrient] = useState({});  // 서버에서 받아온 영양소 정보
+//     const [tdee, setTdee] = useState(1);  // 서버에서 받아온 tdee
+//     const [isLoading, setIsLoading] = useState(true);
+//     const [pieData, setPieData] = useState({
+//         charbodrate : [
+//             {
+//                 value: 0,
+//                 color: "#3366FF",
+//             }, {
+//                 value: 100,
+//                 color: "#DDDDDD"
+//             }
+//         ],
+//         protein : [
+//             {
+//                 value: 0,
+//                 color: "#3366FF",
+//             }, {
+//                 value: 100,
+//                 color: "#DDDDDD"
+//             }
+//         ],
+//         fat : [
+//             {
+//                 value: 0,
+//                 color: "#3366FF",
+//             }, {
+//                 value: 100,
+//                 color: "#DDDDDD"
+//             }
+//         ],
+//     });
 
-    useEffect(() => {
-        const setInfos = async () => {
-            const data = await loadNutrient({ documentKey: userId });
-            console.log("data : ", data);
-            if (Object.keys(data).length === 0) {
-                Alert.alert("오류 발생");
-                return;
-            }
-            setNutrient(data.nutrient);
-            setTdee(Math.floor(data.tdee));
-            setIsLoading(false);
-            console.log(nutrient.calorie/tdee)
-        };
+//     const [barData, setBarData] = useState(
+//         [
+//             {
+//                 value: 0,
+//                 label: '나트륨',
+//                 labelWidth: labelSize,
+//                 labelTextStyle: { color: 'gray' },
+//                 frontColor: '#30A2FF',
+//             },
+//             {
+//                 value: 0,
+//                 label: '포화지방',
+//                 labelWidth: labelSize,
+//                 labelTextStyle: { color: 'gray' },
+//                 frontColor: '#ADC8FF',
+//             },
+//             {
+//                 value: 0,
+//                 label: '트랜스지방',
+//                 labelWidth: labelSize,
+//                 labelTextStyle: { color: 'gray' },
+//                 frontColor: '#84A9FF',
+//             },
+//             {
+//                 value: 0,
+//                 label: "당류",
+//                 labelWidth: labelSize,
+//                 labelTextStyle: { color: 'gray' },
+//                 frontColor: '#6690FF',
+//             },
+//             {
+//                 value: 0,
+//                 label: '칼슘',
+//                 labelWidth: labelSize,
+//                 labelTextStyle: { color: 'gray' },
+//                 frontColor: '#3366FF',
+//             },
+//             {
+//                 value: 0,
+//                 label: '콜레스테롤',
+//                 labelWidth: labelSize,
+//                 labelTextStyle: { color: 'gray' },
+//                 frontColor: '#254EDB',
+//             },
+//         ]
+//     );
 
-        setInfos();
-    }, []);
+//     // TDEE * 0.65 / 4 > 탄수화물
+//     // TDEE * 0.2 / 4 > 단백질
+//     // TDEE * 0.35 / 9 > 지방
 
-    useEffect(() => {
-        console.log(tdee);
-    }, [tdee]);
+//     useEffect(() => {
+//         const setInfos = async () => {
+//             const data = await loadNutrient({ documentKey: userId });
+//             console.log("data : ", data);
+//             if (Object.keys(data).length === 0) {
+//                 Alert.alert("오류 발생");
+//                 return;
+//             }
+//             const newNutrient = data.nutrient
+//             setNutrient(newNutrient);
+//             const newTdee = Math.floor(data.tdee) 
+//             setTdee(newTdee);
+//             console.log(Math.floor(newNutrient.charbodrate / (tdee * 0.65 / 4)))
+//             const newPieData = {
+//                 charbodrate : [
+//                     {
+//                         value: Math.floor(newNutrient.charbodrate / (tdee * 0.65 / 4)),
+//                         color: "#3366FF",
+//                     }, {
+//                         value: 100 - Math.floor(newNutrient.charbodrate / (tdee * 0.65 / 4)),
+//                         color: "#DDDDDD"
+//                     }
+//                 ],
+//                 protein : [
+//                     {
+//                         value: Math.floor(newNutrient.protein / (tdee * 0.2 / 4)),
+//                         color: "#3366FF",
+//                     }, {
+//                         value: 100 - Math.floor(newNutrient.protein / (tdee * 0.2 / 4)),
+//                         color: "#DDDDDD"
+//                     }
+//                 ],
+//                 fat : [
+//                     {
+//                         value: Math.floor(newNutrient.fat / (tdee * 0.35 / 4)),
+//                         color: "#3366FF",
+//                     }, {
+//                         value: 100 - Math.floor(newNutrient.fat / (tdee * 0.35 / 4)),
+//                         color: "#DDDDDD"
+//                     }
+//                 ],
+//             };
+//             setPieData(newPieData);
 
-    useEffect(() => {
-        console.log(nutrient);
-    }, [nutrient]);
+//             const newBarData = [
+//                 {
+//                     value: Math.floor(newNutrient.sodium / 100),
+//                     label: '나트륨',
+//                     labelWidth: labelSize,
+//                     labelTextStyle: { color: 'gray' },
+//                     frontColor: '#30A2FF',
+//                 },
+//                 {
+//                     value: Math.floor(newNutrient.saturatedFat / 100),
+//                     label: '포화지방',
+//                     labelWidth: labelSize,
+//                     labelTextStyle: { color: 'gray' },
+//                     frontColor: '#ADC8FF',
+//                 },
+//                 {
+//                     value: Math.floor(newNutrient.transFat / 100),
+//                     label: '트랜스지방',
+//                     labelWidth: labelSize,
+//                     labelTextStyle: { color: 'gray' },
+//                     frontColor: '#84A9FF',
+//                 },
+//                 {
+//                     value: Math.floor(newNutrient.sugar / 100),
+//                     label: "당류",
+//                     labelWidth: labelSize,
+//                     labelTextStyle: { color: 'gray' },
+//                     frontColor: '#6690FF',
+//                 },
+//                 {
+//                     value: Math.floor(newNutrient.culcium / 100),
+//                     label: '칼슘',
+//                     labelWidth: labelSize,
+//                     labelTextStyle: { color: 'gray' },
+//                     frontColor: '#3366FF',
+//                 },
+//                 {
+//                     value: Math.floor(newNutrient.cholesterol / 100),
+//                     label: '콜레스테롤',
+//                     labelWidth: labelSize,
+//                     labelTextStyle: { color: 'gray' },
+//                     frontColor: '#254EDB',
+//                 },
+//             ];
+//             setBarData(newBarData);
 
-    if (isLoading) {
-        return <LoadingComponent />;
-    }
+//             setIsLoading(false);
+//         };
 
-    const pieData = [
-        {
-            value: 30,
-            color: '#3366FF',
-        },
-        { value: 100, color: '#DDDDDD' },
-    ];
+//         setInfos();
+//     }, []);
 
-    const barData = [
-        {
-            value: 30,
-            label: '나트륨',
-            labelWidth: labelSize,
-            labelTextStyle: { color: 'gray' },
-            frontColor: '#30A2FF',
-        },
-        {
-            value: 30,
-            label: '포화지방',
-            labelWidth: labelSize,
-            labelTextStyle: { color: 'gray' },
-            frontColor: '#ADC8FF',
-        },
-        {
-            value: 30,
-            label: '트랜스지방',
-            labelWidth: labelSize,
-            labelTextStyle: { color: 'gray' },
-            frontColor: '#84A9FF',
-        },
-        {
-            value: 30,
-            label: "당류",
-            labelWidth: labelSize,
-            labelTextStyle: { color: 'gray' },
-            frontColor: '#6690FF',
-        },
-        {
-            value: 30,
-            label: '칼슘',
-            labelWidth: labelSize,
-            labelTextStyle: { color: 'gray' },
-            frontColor: '#3366FF',
-        },
-        {
-            value: 40,
-            label: '콜레스테롤',
-            labelWidth: labelSize,
-            labelTextStyle: { color: 'gray' },
-            frontColor: '#254EDB',
-        },
-    ];
+//     useEffect(() => {
+//         console.log(pieData)
+//     }, [pieData])
 
-    return (
-        <>
-            <View style={[styles.boxContainer]}>
-                <Text style={styles.sectionLabel}>일일 칼로리 섭취량</Text>
-                <View style={styles.chartContainer}>
-                    <Progress.Bar
-                        progress={0 || ( nutrient.calorie / tdee)}
-                        width={screenWidth * 0.8}
-                        color={'#FF6347'}
-                        style={styles.progressBar}
-                    />
-                </View>
-                <View style={{ flexDirection: "row" }}>
-                    <Text style={styles.progressLabel}>{nutrient.calorie && tdee ? Math.floor((nutrient.calorie / tdee) * 100) : 0} %</Text>
-                    <Text style={styles.progressLabel}> {0 || nutrient.calorie} / {tdee} Kcal</Text>
-                </View>
-            </View>
-            <View style={styles.boxContainer}>      
-                <Text style={styles.sectionLabel}>일일 주요 영양소 섭취량</Text>
-                <View style={styles.chartContainer}>
-                    <DonutGraph data={pieData} label="탄수화물" />
-                    <View style={styles.separatorVertical} />
-                    <DonutGraph data={pieData} label="단백질" />
-                    <View style={styles.separatorVertical} />
-                    <DonutGraph data={pieData} label="지방" />
-                </View>
-            </View>
-            <View style={styles.boxContainer}>
-                <Text style={styles.sectionLabel}>일일 기타 영양소 섭취량</Text>
-                <BarGraph data={barData} />
-            </View>
-        </>
-    );
-};
+//     useEffect(() => {
+//         console.log(barData)
+//     }, [barData])
 
-const styles = StyleSheet.create({
-    progressContainer: {
-        alignItems: 'center',
-        marginVertical: 20,
-    },
-    progressLabel: {
-        fontSize: 16,
-        marginTop: 10,
-        fontWeight: 'bold',
-        color: '#333',
-    },
-    sectionLabel: {
-        fontSize: 18,
-        marginVertical: 15,
-        fontWeight: 'bold',
-        color: '#333',
-        textAlign: 'center',
-    },
-    infoBox: {
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        padding: 10,
-    },
-    row: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        width: '100%',
-    },
-    boxContainer: {
-        flexDirection: 'column',
-        justifyContent: 'space-around',
-        padding: 10,
-        alignItems: 'center',
-        marginBottom: 5,
-        marginHorizontal: 5,
-        width: '98%',
-        backgroundColor: '#f8f8f8',
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 5,
-        elevation: 3,
-    },
-    chartContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        width: '98%',
-        padding: 10,
-        backgroundColor: '#f8f8f8',
-        borderRadius: 10,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 5,
-        elevation: 3,
-    },
-    separatorVertical: {
-        width: 1,
-        height: '100%',
-        backgroundColor: '#ccc',
-        marginHorizontal: 10,
-    },
-    barChartContainer: {
-        margin: 20,
-        padding: 20,
-        backgroundColor: '#f8f8f8',
-        borderRadius: 10,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 5,
-        elevation: 3,
-    },
-    progressBar: {
-        borderRadius: 5,
-    },
-});
+//     if (isLoading) {
+//         return <LoadingComponent />;
+//     }
+
+//     return (
+//         <>
+//             <View style={[styles.boxContainer]}>
+//                 <Text style={styles.sectionLabel}>일일 칼로리 섭취량</Text>
+//                 <View style={styles.chartContainer}>
+//                     <Progress.Bar
+//                         progress={0 || ( nutrient.calorie / tdee)}
+//                         width={screenWidth * 0.8}
+//                         color={'#FF6347'}
+//                         style={styles.progressBar}
+//                     />
+//                 </View>
+//                 <View style={{ flexDirection: "row" }}>
+//                     <Text style={styles.progressLabel}>{nutrient.calorie && tdee ? Math.floor((nutrient.calorie / tdee) * 100) : 0} %</Text>
+//                     <Text style={styles.progressLabel}> {0 || nutrient.calorie} / {tdee} Kcal</Text>
+//                 </View>
+//             </View>
+//             <View style={styles.boxContainer}>      
+//                 <Text style={styles.sectionLabel}>일일 주요 영양소 섭취량</Text>
+//                 <View style={styles.chartContainer}>
+//                     <DonutGraph data={pieData.charbodrate} label="탄수화물" />
+//                     <View style={styles.separatorVertical} />
+//                     <DonutGraph data={pieData.protein} label="단백질" />
+//                     <View style={styles.separatorVertical} />
+//                     <DonutGraph data={pieData.fat} label="지방" />
+//                 </View>
+//             </View>
+//             <View style={styles.boxContainer}>
+//                 <Text style={styles.sectionLabel}>일일 기타 영양소 섭취량</Text>
+//                 <BarGraph data={barData} />
+//             </View>
+//         </>
+//     );
+// };
+
+// const styles = StyleSheet.create({
+//     progressContainer: {
+//         alignItems: 'center',
+//         marginVertical: 20,
+//     },
+//     progressLabel: {
+//         fontSize: 16,
+//         marginTop: 10,
+//         fontWeight: 'bold',
+//         color: '#333',
+//     },
+//     sectionLabel: {
+//         fontSize: 18,
+//         marginVertical: 15,
+//         fontWeight: 'bold',
+//         color: '#333',
+//         textAlign: 'center',
+//     },
+//     infoBox: {
+//         justifyContent: 'space-around',
+//         alignItems: 'center',
+//         padding: 10,
+//     },
+//     row: {
+//         flexDirection: 'row',
+//         justifyContent: 'space-around',
+//         alignItems: 'center',
+//         width: '100%',
+//     },
+//     boxContainer: {
+//         flexDirection: 'column',
+//         justifyContent: 'space-around',
+//         padding: 10,
+//         alignItems: 'center',
+//         marginBottom: "4%",
+//         marginHorizontal: 5,
+//         width: '98%',
+//         backgroundColor: '#EFf2EA',
+//         shadowColor: '#000',
+//         shadowOffset: { width: 0, height: 2 },
+//         shadowOpacity: 0.1,
+//         shadowRadius: 5,
+//         elevation: 3,
+//     },
+//     chartContainer: {
+//         flexDirection: 'row',
+//         justifyContent: 'space-around',
+//         alignItems: 'center',
+//         width: '98%',
+//         padding: 10,
+//         backgroundColor: '#f8f8f8',
+//         borderRadius: 10,
+//         shadowColor: '#000',
+//         shadowOffset: { width: 0, height: 2 },
+//         shadowOpacity: 0.1,
+//         shadowRadius: 5,
+//         elevation: 3,
+//     },
+//     separatorVertical: {
+//         width: 1,
+//         height: '100%',
+//         backgroundColor: '#ccc',
+//         marginHorizontal: 10,
+//     },
+//     barChartContainer: {
+//         margin: 20,
+//         padding: 20,
+//         backgroundColor: '#f8f8f8',
+//         borderRadius: 10,
+//         shadowColor: '#000',
+//         shadowOffset: { width: 0, height: 2 },
+//         shadowOpacity: 0.1,
+//         shadowRadius: 5,
+//         elevation: 3,
+//     },
+//     progressBar: {
+//         borderRadius: 5,
+//     },
+// });
 
 module.exports = { GraphPage };

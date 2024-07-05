e97646108b9b21247d313abfa4313ee8a8110351
minkinghub <kp141@naver.com>
2024-07-05 10:18:16 +0900

    upstage

--- back/controllers/upload_controller.py
+++ back/controllers/upload_controller.py
@@ -1,39 +1,36 @@
 import base64
-from PIL import Image
-from io import BytesIO
+import requests
 from fastapi import HTTPException
-from google.cloud import vision
 from models.upload_model import ImageBase64
 
 def upload_controller(image: ImageBase64):
     try:
+        # base64 문자열을 디코딩
         image_data = base64.b64decode(image.base64)
         if not image_data:
-            raise ValueError("Decoded image data is empty.")       
-        
-        pil_image = Image.open(BytesIO(image_data))
+            raise ValueError("Decoded 이미지 데이터가 비어 있습니다.")
     except Exception as e:
-        print(f"Error decoding image: {e}")  
-        raise HTTPException(status_code=400, detail={"code": "Io2", "message": "Error decoding image"})
+        print(f"이미지 디코딩 오류: {e}")
+        raise HTTPException(status_code=400, detail={"code": "Io2", "message": "이미지 디코딩 오류"})
+
+    # 업스테이지 OCR API 요청
+    api_key = "up_ZrP7tTb7Hc7UhrdIZulcotZN8szlj"
+    url = "https://api.upstage.ai/v1/document-ai/ocr"
+    headers = {"Authorization": f"Bearer {api_key}"}
     
-    # Google Cloud Vision API 클라이언트 생성
-    client = vision.ImageAnnotatorClient()
+    try:
+        files = {"document": ("image.jpg", image_data, "application/octet-stream")}
+        response = requests.post(url, headers=headers, files=files)
+    except Exception as e:
+        print(f"이미지 읽기 또는 요청 전송 오류: {e}")
+        raise HTTPException(status_code=400, detail={"code": "Io2", "message": "이미지 읽기 또는 요청 전송 오류"})
 
-    # 이미지를 Google Cloud Vision API에서 인식할 수 있는 형식으로 변환
-    image = vision.Image(content=image_data)
-
-    # OCR 수행
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
-
-    if response.error.message:
-        raise HTTPException(status_code=400, detail={"code": "I02", "message": "Error OCR process"})
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail={"code": "I02", "message": "Error OCR process"})
+    
+    response_data = response.json()
 
     # OCR 결과 추출
-    if texts:
-        ocr_result = texts[0].description
-    else:
-        ocr_result = ""
-    
-    print(ocr_result)
+    ocr_result = response_data.get("text", "")
+
     return ocr_result
--- back/routers/upload_router.py
+++ back/routers/upload_router.py
@@ -9,23 +9,25 @@ from controllers.auth_controller import get_current_user_id
 router = APIRouter()
 
 """
-#대한민국 #대표콘 #부라보콘
-•총 내용량 140ml 230 kcal
-납자는 바닐라맛을
-영양정보
-라빈시드입니다.
-천천히 녹여드십시오
-환경을 깨끗이
-O
-나트륨 65 mg 3% 탄수화물 32 g 10% 당류 19 g 19% 지방9g17%
-트랜스지방 0g 포화지방 5g33% 콜레스테롤 10 mg 3% 단백질 5g9%
-1일 영양성분 기준치에 대한 비율(%)은 2,000 kcal 기준이므로 개인의 필요 열량에 따라 다를 수 있습니다.
-O
+총 내용량 74g(18.5gx 4봉지) 
+영양정보 1봉지(18.5g)당 85kcal/ 총 343kcal
+1봉지당 1일 영양성분 기준치에대한비율 총 내용량당
+나트륨 85 mg 4% 338 mg 17%
+탄수화물 14 g 4% 54 g 17%
+당류 2 g 2% 9 g 9%
+지방 2.6 g 5% 11 g 20%
+트랜스지방 0 g 0 g
+포화지방 0.7 g 5% 2.8 g 19%
+콜레스테롤 4 mg 1% 14 mg 5%
+단백질 2g 4% 7 g 13%
+1일 영양성분 기준치에 대한 비율(%)은 2,000kcal
+기준이므로 개인의 필요 열량에 따라 다를 수 있습니다.
 """
 
 @router.post("/upload")
 async def upload_router(image: ImageBase64):
     text = upload_controller(image)  # 이미지에서 텍스트를 추출하는 함수 호출
+    print(text)
     
     if not text:
         raise HTTPException(status_code=400, detail="No text extracted from image")
